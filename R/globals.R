804ddaf8b5785720450dc01ac1589e95cbc7223c
Julia Silge <julia.silge@gmail.com>
2020-05-07 19:54:58 -0600

    Switch out globals

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 globalVariables(c(
-    "freq1", "freq2", "freqnotthem", "freq2notthem", "l1them", "l2notthem",
-    "sigma2", "total"
+    "alpha", "delta_wi", "n_i", "omega_w", "omega_wi", "sigma2_wi",
+    "y_w", "y_wi", "zeta_wi"
 ))
